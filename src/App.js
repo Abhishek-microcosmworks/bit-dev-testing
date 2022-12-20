dc0deda9f2acd67fb350e6a75e2c636a7f556d4a
Abhishek-microcosmworks <abhishek@microcosmworks.com>
2022-12-20 16:10:13 +0530

    Bit.dev Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import '@fontsource/noto-sans';
-import Source from './components/Source/index';
+import axios from 'axios';
+import { Source } from '@vlogrinc/vlogr.components-lib';
+import GooglePhotoIcon from './assets/images/Group 308.png';
+import LinkBreak from './assets/images/LinkBreak.png';
+import spinner  from './assets/images/spinner.gif';
+import PlayIcon from './assets/images/Vector.png';
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_PHOTOS_CLIENT_ID;
+const API_KEY = process.env.REACT_APP_GOOGLE_PHOTOS_API_KEY;
+const REDIRECT_URI = process.env.REACT_APP_GOOGLE_PHOTOS_REDIRECT_URI;
 
 function App() {
   return (
     <div className="App">
-      <Source />
+      <Source
+        clientId={CLIENT_ID} 
+        apiKey={API_KEY} 
+        redirectUri={REDIRECT_URI}
+        googlePhotoIcon={GooglePhotoIcon}
+        linkBreak={LinkBreak}
+        spinner={spinner}
+        playIcon={PlayIcon}
+        axios={axios}
+      />
     </div>
   );
 }
