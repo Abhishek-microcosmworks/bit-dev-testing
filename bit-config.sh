cd029fc34eb91ebd667c1a15da8da23a2fdb0426
Abhishek-microcosmworks <abhishek@microcosmworks.com>
2023-01-09 16:39:00 +0530

    added workspace

---
+++ bit-config.sh
@@ -0,0 +1,5 @@
+bit config set analytics_reporting false
+bit config set anonymous_reporting false
+bit config set user.token aab3f670-3acb-4ccc-b367-1e90a9a556e6
+echo @bit:registry=https://node.bit.dev >> .npmrc
+echo //node.bit.dev/:_authToken=aab3f670-3acb-4ccc-b367-1e90a9a556e6 >> .npmrc
