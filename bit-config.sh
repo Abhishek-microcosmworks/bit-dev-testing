379b7c2b20e2e24eb86d7f0bc0fb50ed5f9d60f8
Abhishek-microcosmworks <abhishek@microcosmworks.com>
2023-01-09 16:47:09 +0530

    changes

--- bit-config.sh
+++ bit-config.sh
@@ -1,5 +1,5 @@
 bit config set analytics_reporting false
 bit config set anonymous_reporting false
-bit config set user.token aab3f670-3acb-4ccc-b367-1e90a9a556e6
+bit config set user.token ${BIT_TOKEN}
 echo @bit:registry=https://node.bit.dev >> .npmrc
-echo //node.bit.dev/:_authToken=aab3f670-3acb-4ccc-b367-1e90a9a556e6 >> .npmrc
+echo //node.bit.dev/:_authToken=${BIT_TOKEN} >> .npmrc
