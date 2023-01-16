4ef756dbb4f81fd268f8a6a53f52f2ca66c3562a
Abhishek-microcosmworks <abhishek@microcosmworks.com>
2023-01-16 18:02:04 +0530

    Added .npmrc

--- bit-config.sh
+++
@@ -1,5 +0,0 @@
-bit config set analytics_reporting false
-bit config set anonymous_reporting false
-bit config set user.token ${BIT_TOKEN}
-echo @bit:registry=https://node.bit.dev >> .npmrc
-echo //node.bit.dev/:_authToken=${BIT_TOKEN} >> .npmrc
